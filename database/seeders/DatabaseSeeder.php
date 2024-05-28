a5798a9dd7653da7b1a866d225f00459562563bc
Igor Santos <iggor.jsantos@gmail.com>
2024-05-28 15:54:16 -0300

    criação das tabelas e dos factories e seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(CategoriasSeeder::class);
+        $this->call(ProdutosSeeder::class);
     }
 }
