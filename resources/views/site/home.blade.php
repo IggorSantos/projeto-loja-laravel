8ce4717747510ca24806241baf85918cb7b29154
Igor Santos <iggor.jsantos@gmail.com>
2024-06-05 09:50:50 -0300

    add categories

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -5,17 +5,16 @@
 <div class="container">
     <div class="row">
         @foreach($produtos as $produto)
-        <div class="card col-md-3 mt-3">
-            <img src="{{ $produto->imagem }}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">{{ $produto->nome }}</h5>
-            <p class="card-text">{{ Str::limit($produto->descricao, 10) }}</p>
-            <a href="#" class="btn btn-primary">Comprar</a>
+            <div class="card col-md-3 mt-3">
+                <img src="{{ $produto->imagem }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">{{ Str::limit($produto->nome, 20) }}</h5>
+                <p class="card-text">{{ Str::limit($produto->descricao, 10) }}</p>
+                <a href="/produtos/{{ $produto->slug }}" class="btn btn-primary">Comprar</a>
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
     </div>
-
 </div>
 
 
--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -9,11 +9,36 @@
         
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
+    <link rel="stylesheet" href="/css/style.css">
+
     <title>Produtos</title>
 </head>
 <body>
-
+    <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">              
+              <div class="dropdown">
+                <button class="button-drop btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Categorias
+                </button>
+                <ul class="dropdown-menu">
+                  @foreach($categoriasMenu as $categoriaM)
+                    <li><a class="dropdown-item" href="#">{{ $categoriaM->nome }}</a></li>
+                  @endforeach               
+                </ul>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Carrinho</a>
+            </li>
+    </ul>
 @yield('conteudo')
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
     
 </body>
 </html>
